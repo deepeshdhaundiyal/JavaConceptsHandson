7c5d0e791b51fceb3f797b0d4523ca2228447a15
deepeshdhaundiyal <deepeshdhaundiyal@gmail.com>
2025-03-21 14:52:30 +0530

    Committing my code for two strings are Anagram

---
+++ src/main/java/concepts/handson/Multithreading/InterThreadCommunication/TwoAnagramString.java
@@ -0,0 +1,30 @@
+package concepts.handson.Multithreading.InterThreadCommunication;
+
+import java.util.Arrays;
+
+public class TwoAnagramString {
+
+    private static boolean isStringEqual(String a, String b){
+
+        if(a.length() != b.length()){
+            return false;
+        }
+
+         char [] aa = a.toCharArray();
+         char [] bb = b.toCharArray();
+
+         Arrays.sort(aa);
+         Arrays.sort(bb);
+         return Arrays.equals(aa,bb);
+
+    }
+    public static void main(String[] args) {
+
+        String a = "slisten";
+        String b = "silents";
+
+        System.out.println(TwoAnagramString.isStringEqual(a,b));
+
+    }
+
+}
